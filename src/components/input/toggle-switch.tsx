94dcac5e85b25a8a3f12a27792f41dfc4a6324da
Diego Cisneros <diegocl02@gmail.com>
2020-10-19 22:05:52 -0300

    update

--- src/components/input/toggle-switch.tsx
+++ src/components/input/toggle-switch.tsx
@@ -42,82 +42,149 @@ export const ToggleSwitch: Component<Props, Messages> = (props) => {
 	const sliderWidth = style.height
 	// const borderColor = colorLuminance(config.theme.colors.whitish, -0.1)
 
-	return <StackPanel orientation={"horizontal"} style={{ height: "100%", ...props.style }}>
-		{
-			props.choices.map((option, index) => {
-				const IconItem = option.icon
-				const border = selectedSwitchIndex === index
-					? `2px solid`
-					: `thin solid`
+	return choices.length == 2
+		? <HoverBox
+			// theme={config.theme}
+			style={{
+				display: "flex",
+				flexDirection: selectedSwitchIndex === 1 ? "row-reverse" : "row",
+				// backgroundColor: theme.colors.secondary.light,
+				// ...type !== "multiple-choices" && selectedSwitchIndex === 0
+				// 	? { backgroundColor: theme.colors.primary.dark, }
+				// 	: {},
+				width: "90px",
+				borderRadius: "20px",
+				margin: "2px",
+				boxShadow: "inset 0px 1px 1px rgba(0,0,0,0.4)",
+				cursor: "pointer",
+				...style
+			}}
+			hoverStyle={{
+				display: "flex",
+				flexDirection: selectedSwitchIndex === 1 ? "row-reverse" : "row",
+				// backgroundColor: theme.colors.secondary.dark,
+				// ...type !== "multiple-choices" && selectedSwitchIndex === 0
+				// 	? { backgroundColor: theme.colors.primary.dark }
+				// 	: {},
+				...style
+			}}>
 
-				return <HoverBox
+			<div
+				onClick={() => {
+					if (postMsgAsync) {
+						postMsgAsync({
+							type: "SWITCH_CHANGE",
+							data: {
+								index: props.selectedSwitchIndex === 0 ? 1 : 0
+							}
+						})
+					}
+				}}>
+				<div
 					style={{
-						backgroundColor: "white",
-						color: props.selectedSwitchIndex === index
-							? 'white'
-							: "black",
-						height: "100%",
-						flex: "1"
-					}}
-					hoverStyle={{
-						backgroundColor: option.isDisabled !== true
-							? "whitesmoke"
-							: "inherit",
-						borderColor: props.style && props.style.borderColor
+						height: sliderWidth,
+						width: sliderWidth,
+						borderRadius: "30px",
+						// backgroundColor: theme.colors.whitish,
+						boxShadow: "0px 1px 5px rgba(0,0,0,0.5)",
+						transform: "scale(0.8)"
 					}}>
+				</div>
+				<div
+					style={{
+						width: "calc(100% - 40px)",
+						justifyContent: "center",
+						color: "white",
+						display: "flex",
+						alignItems: "center",
+						whiteSpace: "nowrap",
+						overflow: "hidden",
+						textOverflow: "ellipsis",
+						height: "100%",
+						textShadow: `0px 1px 2px rgba(0,0,0,0.2)`
+					}}>
+					{props.choices[selectedSwitchIndex].label}
+				</div>
+			</div>
+		</HoverBox>
+		: <StackPanel orientation={"horizontal"} style={{ height: "100%", ...props.style }}>
+			{
+				props.choices.map((option, index) => {
+					const IconItem = option.icon
+					const border = selectedSwitchIndex === index
+						? `2px solid`
+						: `thin solid`
 
-					<StackPanel
-						title={option.tooltip}
-						itemsAlignV={"center"}
+					return <HoverBox
+						// theme={config.theme}
 						style={{
-							cursor: option.isDisabled !== true ? "pointer" : "inherit",
-							borderRight: selectedSwitchIndex === index + 1
-								? `2px solid`
-								: border
-							,
-							borderTop: border,
-							borderBottom: border,
-							// ...props.selectedSwitchIndex === index && { boxShadow: "inset 0 0 2px #333" },
-							...index === 0
-							&& { borderLeft: border, borderRadius: "0.3rem 0 0 0.3rem" },
-							...index === props.choices.length - 1
-							&& { borderRadius: "0 0.3rem 0.3rem 0" },
-							...option.style,
-
+							backgroundColor: "white",
+							color: props.selectedSwitchIndex === index
+								? 'white'
+								: "black",
+							// userSelect: "none",
+							height: "100%",
+							flex: "1"
 						}}
-						onClick={() => {
-							if (props.postMsgAsync && option.isDisabled !== true)
-								props.postMsgAsync({
-									type: "SWITCH_CHANGE",
-									data: {
-										index: index
-									}
-								})
+						hoverStyle={{
+							backgroundColor: option.isDisabled !== true
+								? "whitesmoke"
+								: "inherit",
+							borderColor: props.style && props.style.borderColor
 						}}>
-						{
-							option.customElement
-								? <StackPanel
-									style={{
-										height: '100%',
-										width: "100%",
-										margin: "auto"
-									}}
-									itemsAlignV={"center"}
-									itemsAlignH={"center"}>
-									{option.customElement}
-								</StackPanel>
-								: <StackPanel
-									style={{ height: '100%', width: "100%", margin: "auto", }}
-									itemsAlignV={"center"}
-									itemsAlignH={"center"}>
-									{IconItem && <IconItem style={{}} />}
-									{option.label}
-								</StackPanel>
-						}
-					</StackPanel>
-				</HoverBox>
-			})
-		}
 
-	</StackPanel>
+						<StackPanel
+							title={option.tooltip}
+							itemsAlignV={"center"}
+							style={{
+								cursor: option.isDisabled !== true ? "pointer" : "inherit",
+								borderRight: selectedSwitchIndex === index + 1
+									? `2px solid`
+									: border
+								,
+								borderTop: border,
+								borderBottom: border,
+								// ...props.selectedSwitchIndex === index && { boxShadow: "inset 0 0 2px #333" },
+								...index === 0
+								&& { borderLeft: border, borderRadius: "0.3rem 0 0 0.3rem" },
+								...index === props.choices.length - 1
+								&& { borderRadius: "0 0.3rem 0.3rem 0" },
+								...option.style,
+
+							}}
+							onClick={() => {
+								if (props.postMsgAsync && option.isDisabled !== true)
+									props.postMsgAsync({
+										type: "SWITCH_CHANGE",
+										data: {
+											index: index
+										}
+									})
+							}}>
+							{
+								option.customElement
+									? <StackPanel
+										style={{
+											height: '100%',
+											width: "100%",
+											margin: "auto"
+										}}
+										itemsAlignV={"center"}
+										itemsAlignH={"center"}>
+										{option.customElement}
+									</StackPanel>
+									: <StackPanel
+										style={{ height: '100%', width: "100%", margin: "auto", }}
+										itemsAlignV={"center"}
+										itemsAlignH={"center"}>
+										{IconItem && <IconItem style={{}} />}
+										{option.label}
+									</StackPanel>
+							}
+						</StackPanel>
+					</HoverBox>
+				})
+			}
+
+		</StackPanel>
 }
