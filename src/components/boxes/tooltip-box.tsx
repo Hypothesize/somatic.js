a4e060e875140ebc0f1ed3778967dfac0cdb9aad
Diego Cisneros <diegocl02@gmail.com>
2020-11-18 18:01:47 -0500

    update

--- src/components/boxes/tooltip-box.tsx
+++ src/components/boxes/tooltip-box.tsx
@@ -275,7 +275,7 @@ export const TooltipBox: Component<Props> = async (props) => {
 		</span>
 	}
 	else { // We indicate that on element load, we need to apply tooltips
-		return <span id={tooltipId} style={{ ...style }}>
+		return <span id={tooltipId}>
 			<style onLoad={() => { assignTooltips(tooltipId) }} />
 			{children}
 		</span>
