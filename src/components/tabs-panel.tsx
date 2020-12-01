17301384c170bfaa8898ab88d2890e3a04928d42
James Prempeh <agyemanjp@danfortsystems.com>
2020-12-01 13:21:53 +0000

    updates

--- src/components/tabs-panel.tsx
+++ src/components/tabs-panel.tsx
@@ -1,12 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import { deepMerge } from '@sparkwave/standard/collections/object'
-import { RecursivePartial } from '@sparkwave/standard'
+import { deepMerge, } from '@sparkwave/standard/collections/object'
 
 import { StackView, Props as StackViewProps } from './stack-view'
 import { StackPanel, Props as StackPanelProps } from './stack-panel'
 import { Component, CSSProperties, HtmlProps, ViewProps } from '../types'
-import { createElement } from '../core'
+import { createElement, makeComponent } from '../core'
 
 
 export type Messages = (
@@ -18,7 +17,7 @@ export type Props = HtmlProps & {
 	selectedIndex?: number
 }
 
-export const TabsPanel: Component<Props, Messages> = (props) => {
+export const TabsPanel = makeComponent({})<Props>(async (props) => {
 
 	const defaultProps/*: RecursivePartial<Props>*/ = {
 		selectedIndex: 0,
@@ -56,5 +55,5 @@ export const TabsPanel: Component<Props, Messages> = (props) => {
 			{(children ?? [])[selectedIndex]}
 		</div>
 	</StackPanel>
-}
+})
 
