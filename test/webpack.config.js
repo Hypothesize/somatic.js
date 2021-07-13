e7c00d621d5f358d509a1b3a6627206a206cf5df
Arthur <arthur.plouet@gmail.com>
2021-07-13 17:46:43 +0800

    Update

---
+++ test/webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	context: __dirname,
+	entry: './dist/test.index.js',
+	output: {
+		path: __dirname + '/dist',
+		filename: 'index.bundle.js'
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	node: {
+		fs: 'empty',
+		net: 'empty',
+		tls: 'empty',
+	},
+	target: 'web'
+}
