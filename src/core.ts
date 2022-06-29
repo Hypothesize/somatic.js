9d52e41fe36919c1abd8a5ee39269c111567248c
eltharim <arthur.plouet@gmail.com>
2022-06-29 14:54:19 +0800

    First commit

--- src/core.ts
+++ src/core.ts
@@ -216,6 +216,7 @@ export async function updateChildrenAsync(eltDOM: DOMElement | DocumentFragment,
 	const matching = (dom: Node, elt: UIElement) => {
 		return isAugmentedDOM(dom)
 			&& isIntrinsicElt(dom.renderTrace.leafElement)
+			&& typeof dom.renderTrace.leafElement.props === "object"
 			&& "key" in dom.renderTrace.leafElement.props
 			&& isEltProper(elt)
 			&& "key" in elt.props
